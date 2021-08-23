796e635c0e925ac2e69409f7a0a74acb74c1593c
Bill Bejeck <bbejeck@gmail.com>
2021-08-23 16:53:36 -0400

    MINOR: Add properties to json task, individual scripts for AWS actions

--- aws-cli/single-commands.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/zsh
-
-. ./configs.sh
-
-aws lambda update-function-code --profile "${PROFILE}" --region "${REGION}" \
-    --function-name ccloudStockTradeFunction \
-    --zip-file fileb://../build/distributions/confluent-lambda-serverless-1.0-SNAPSHOT.zip
