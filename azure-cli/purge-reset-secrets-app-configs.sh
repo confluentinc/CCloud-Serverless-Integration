0528e47095a159d516899eee13d430b4884247c0
Bill Bejeck <bbejeck@gmail.com>
2022-02-18 10:46:24 -0500

    formatting of script

--- azure-cli/purge-reset-secrets-app-configs.sh
+++ azure-cli/purge-reset-secrets-app-configs.sh
@@ -12,8 +12,15 @@ OLD_KEYS=$(az keyvault secret list --vault-name "$KEY_VAULT" | jq -r '.[].name')
 if [ -n "$OLD_KEYS" ]; then
   echo "Retrieved previous secret keys for deleting ${OLD_KEYS}"
   echo "Removing the refs for secrets in applications"
-  az functionapp config appsettings delete --name "$DIRECT_FUNCTION_NAME" --resource-group "$RESOURCE_GROUP" --setting-names $OLD_KEYS
-  az functionapp config appsettings delete --name "$SINK_FUNCTION_NAME" --resource-group "$RESOURCE_GROUP" --setting-names $OLD_KEYS
+  az functionapp config appsettings delete \
+     --name "$DIRECT_FUNCTION_NAME" \
+     --resource-group "$RESOURCE_GROUP" \
+     --setting-names $OLD_KEYS
+
+  az functionapp config appsettings delete \
+     --name "$SINK_FUNCTION_NAME" \
+     --resource-group "$RESOURCE_GROUP" \
+     --setting-names $OLD_KEYS
 
   echo "Deleting all secrets"
   for key in $OLD_KEYS; do
@@ -46,15 +53,25 @@ for k in $(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' azure-app-se
   CONFIG_NAME=$(echo "$k" | cut -d '=' -f 1)
   CONFIG_VALUE=$(echo "$k" | cut -d '=' -f 2)
   echo "Setting config ${CONFIG_NAME} with JSON file ${CONFIG_VALUE}"
-  az keyvault secret set --vault-name "$KEY_VAULT" --name "$CONFIG_NAME" --file "$CONFIG_VALUE"
+  az keyvault secret set \
+    --vault-name "$KEY_VAULT" \
+    --name "$CONFIG_NAME" \
+    --file "$CONFIG_VALUE"
 done
 
 echo "Now setting the application configs"
 for k in $(az keyvault secret list --vault-name "$KEY_VAULT" | jq -r '.[].name'); do
   echo "Setting App setting to latest for ${k} in ${DIRECT_FUNCTION_NAME}"
-  az functionapp config appsettings set --name "$DIRECT_FUNCTION_NAME" --resource-group "$RESOURCE_GROUP" --settings "${k}=@Microsoft.KeyVault(SecretUri=https://confluent-cloud-keyvault.vault.azure.net/secrets/${k})"
+  az functionapp config appsettings set \
+    --name "$DIRECT_FUNCTION_NAME" \
+    --resource-group "$RESOURCE_GROUP" \
+    --settings "${k}=@Microsoft.KeyVault(SecretUri=https://confluent-cloud-keyvault.vault.azure.net/secrets/${k})"
+    
   echo "Now in ${SINK_FUNCTION_NAME}"
-  az functionapp config appsettings set --name "$SINK_FUNCTION_NAME" --resource-group "$RESOURCE_GROUP" --settings "${k}=@Microsoft.KeyVault(SecretUri=https://confluent-cloud-keyvault.vault.azure.net/secrets/${k})";
+  az functionapp config appsettings set \
+    --name "$SINK_FUNCTION_NAME" \
+    --resource-group "$RESOURCE_GROUP" \
+    --settings "${k}=@Microsoft.KeyVault(SecretUri=https://confluent-cloud-keyvault.vault.azure.net/secrets/${k})";
   
 done
 
