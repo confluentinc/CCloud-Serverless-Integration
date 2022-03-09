23e22bcad8477467018e614d98b109529a5319ba
Bill Bejeck <bbejeck@gmail.com>
2022-03-09 17:05:28 -0500

    Updates to Azure scripts for automation

--- azure-cli/azure-create-all.sh
+++ azure-cli/azure-create-all.sh
@@ -1,6 +1,27 @@
 #! /bin/bash
 
-. ./local-azure-configs.sh
+if [ ! -f "azure-app-settings.json" ]; then
+  echo "Missing required config file azure-app-settings.json"
+  exit 1
+fi
 
+. ./azure-configs.sh
 
-az login
+echo "Creating the function app premium plan"
+az functionapp plan create \
+  --name "$PLAN_NAME" \
+  --resource-group "$RESOURCE_GROUP" \
+  --location "$REGION" \
+  --max-burst 100 \
+  --sku EP1
+
+echo "Pause 5 seconds for the functionapp plan to sync"
+sleep 5
+
+echo "Creating the functionapp $DIRECT_FUNCTION_NAME"
+az functionapp create \
+  --name "$DIRECT_FUNCTION_NAME" \
+  --storage-account "$STORAGE_ACCOUNT" \
+  --plan "$PLAN_NAME" \
+  --resource-group "$RESOURCE_GROUP" \
+  --functions-version 4
