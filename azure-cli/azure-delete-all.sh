96a71faad293777af04bf163b64ab6f58dd92bd6
Bill Bejeck <bbejeck@gmail.com>
2022-03-09 17:26:08 -0500

    Configs for subscription name and started clean up script deleting plan and functionapp

--- azure-cli/azure-delete-all.sh
+++ azure-cli/azure-delete-all.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-. ./local-azure-configs.sh
+if [ ! -f "azure-app-settings.json" ]; then
+  echo "Missing required config file azure-app-settings.json"
+  exit 1
+fi
+
+. ./azure-configs.sh
+
+
+echo "Deleting the functionapp"
+
+  az functionapp delete \
+    --name "$DIRECT_FUNCTION_NAME" \
+    --resource-group "$RESOURCE_GROUP" \
+    --subscription "$SUBSCRIPTION_NAME"
+
+echo "Now Deleting the functionapp plan, otherwise leaving the plan could incur more charges"
+ az functionapp plan delete \
+     --name "$PLAN_NAME" \
+     --resource-group "$RESOURCE_GROUP" \
+     --subscription "$SUBSCRIPTION_NAME" \
+     --yes
